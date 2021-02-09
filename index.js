fbe76f0e346def284073d5c6ae6414b2406fdb00
Kolton Decker <deckerman25@gmail.com>
2021-02-08 18:02:28 -0600

    Added packages required and added questions to ask user to array.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your project title?",
+    "What is your project description?",
+    "What are the installation instructions?",
+    "What is the usage information?",
+    "What are the contribution guidelines?",
+    "What are the test instructions?",
+    "Please select the license for your project.",
+    "What's your github username?",
+    "What is your email?"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
